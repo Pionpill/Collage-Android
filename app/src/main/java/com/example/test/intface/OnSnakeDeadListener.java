3d658b4dfce4aa355177cc7d1e88667af75b6764
Pionpill <673486387@qq.com>
2022-06-12 16:01:07 +0800

    finfish

---
+++ app/src/main/java/com/example/test/intface/OnSnakeDeadListener.java
@@ -0,0 +1,5 @@
+package com.example.test.intface;
+
+public interface OnSnakeDeadListener {
+    void OnSnakeDead(int foodcnt);
+}
---
+++ app/src/main/java/com/example/test/intface/OnSnakeEatFoodListener.java
@@ -0,0 +1,5 @@
+package com.example.test.intface;
+
+public interface OnSnakeEatFoodListener {
+    void OnSnakeEatFood(int foodcnt);
+}
